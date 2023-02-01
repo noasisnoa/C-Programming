7eb9e240b845ebb6396d8475be6caf5d9bf80337
noasisnoa <106498526+noasisnoa@users.noreply.github.com>
2023-02-01 16:29:58 +0900

    Add files via upload

---
+++ 2023_01_30/평균필터.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int k=1;
+float x_avg_k_1=0;
+float x_avg_k=0;
+
+void Average(float x_measure)
+	{
+  	x_avg_k =  (x_avg_k_1*(k-1) + x_measure)/k;
+	k++;
+	x_avg_k_1=x_avg_k;
+	}
+
+
+
+int main(void){
+	k=1;
+	float n;
+	int i; //반복 변수
+	while(1){
+		printf("넣고 싶은 수를 넣으시오 : ");
+		scanf("%f",&n);
+		
+		Average(n);
+		
+		//k++;
+		printf("%f \n",x_avg_k);		
+	}
+
+	return 0;
+} 
